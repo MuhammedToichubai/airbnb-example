bd1310f9bff2336dab30f70b5fb42f8453948570
Muhammed <muhammedtoichubai@gmail.com>
2022-07-08 03:39:30 -0600

    Initial commit

---
+++ src/main/java/peaksoft/airbnbexample/AirbnbExampleApplication.java
@@ -0,0 +1,13 @@
+package peaksoft.airbnbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbExampleApplication.class, args);
+    }
+
+}
