07700611eb7cef33a0340bad0d80ad7e13d1f7a8
Kanatbey <98470964+Kanatbey@users.noreply.github.com>
2022-07-17 02:21:23 +0600

    commit

--- src/main/java/peaksoft/airbnbexample/dto/user/UserRequest.java
+++ src/main/java/peaksoft/airbnbexample/dto/user/UserRequest.java
@@ -4,6 +4,8 @@ package peaksoft.airbnbexample.dto.user;
  * @author Muhammed Toichubai
  */
 public class UserRequest {
+    private String fullName;
     private String email;
     private String password;
+    private String image;
 }
